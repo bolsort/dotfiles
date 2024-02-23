f4a827cc8c2b659830fa70432efe87e25d09bf66
Boris Lassort <163381+borislassort@users.noreply.github.com>
2024-02-23 22:59:25 +0100

    vim theme catppuccin

---
+++ nvim/.config/nvim/lua/plugins/catpuccin.lua
@@ -0,0 +1,21 @@
+return { 
+  "catppuccin/nvim", 
+  name = "catppuccin", 
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      }
+    })
+    vim.cmd 'colorscheme catppuccin-mocha'
+  end
+}
