9b7d797a777fbdc889ad2af9415ffcb85b0128cf
Boris Lassort <163381+borislassort@users.noreply.github.com>
2024-02-25 11:29:38 +0100

    nvim autocompletion

---
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "",
+            package_pending = "",
+            package_uninstalled = "",
+          },
+        }
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local mason_lspconfig = require 'mason-lspconfig'
+      local lspconfig = require("lspconfig")
+
+      mason_lspconfig.setup {
+        ensure_installed = { 'pyright', 'tsserver', 'html' }
+      }
+
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+
+}
