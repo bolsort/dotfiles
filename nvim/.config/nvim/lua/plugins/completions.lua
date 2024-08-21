349b5ab47588993d817a33b47e8728d05e0f849a
Boris Lassort <163381+borislassort@users.noreply.github.com>
2024-08-21 15:01:49 +0200

    nvim fix cmd completions

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -5,6 +5,7 @@ return {
   {'hrsh7th/cmp-vsnip'},     
   {'hrsh7th/cmp-path'},                            
   {'hrsh7th/cmp-buffer'},                            
+  {'hrsh7th/cmp-cmdline'},
   {'hrsh7th/vim-vsnip'}, 
   {
     "L3MON4D3/LuaSnip",
