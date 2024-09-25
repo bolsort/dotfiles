115192fa83e544bafe4fef6fd81c139eb0d9cc20
Boris Lassort <163381+borislas@users.noreply.github.com>
2024-09-25 08:44:15 +0200

    nvim add orgmode

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,17 +4,12 @@ return {
   config = function () 
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-        ensure_installed = {
-          "c", "cpp", "nasm", "rust", "go", "lua", "make", "zig",
-          "bash", "latex", "lua", "python", "regex", "java",
-          "json", "terraform", "toml", "yaml",
-          "javascript", "html", "typescript",
-          "gitcommit", "gitignore", "markdown",
-        },
-        auto_install = true,
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
+      ensure_installed = 'all',
+      ignore_install = { 'org' },
+      auto_install = true,
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },  
       })
   end
 }
