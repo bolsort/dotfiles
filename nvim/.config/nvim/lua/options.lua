f75235299572e998ca2a4860ecd7c16624de44ed
Boris Lassort <163381+borislassort@users.noreply.github.com>
2024-10-08 17:37:11 +0200

    neovim - Leader and localLeader key map to " "

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -12,5 +12,9 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 vim.wo.number = true
 vim.wo.relativenumber = true
+
