43e392226a3d3c3d72e4d2ed238d5f1df922c40f
Boris Lassort <163381+borislassort@users.noreply.github.com>
2024-10-30 14:05:42 +0100

    nvim add lazygit to toggleterm : <super>g

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,7 +1,34 @@
  return {
    'akinsho/toggleterm.nvim',
    version = "*",
-   opts = {
-     open_mapping = [[<C-_>]],
-   },
+   config = function()
+     require("toggleterm").setup{
+       open_mapping = [[<C-_>]],
+     }
+
+     local Terminal  = require('toggleterm.terminal').Terminal
+     -- Lazygit Terminal
+     local lazygit = Terminal:new({
+       cmd = "lazygit",
+       dir = "git_dir",
+       direction = "float",
+       float_opts = {
+         border = "double",
+       },
+       -- function to run on opening the terminal
+       on_open = function(term)
+         vim.cmd("startinsert!")
+         vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+       end,
+       -- function to run on closing the terminal
+       on_close = function(term)
+         vim.cmd("startinsert!")
+       end,
+     })
+     function _lazygit_toggle()
+       lazygit:toggle()
+     end
+     vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+   end
  }
+
